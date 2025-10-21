a38c537f1515a03a1b0b13526704e1dfc414d593
DSM3 <fatec-dsm3@academico.intra>
2025-10-21 19:57:25 -0300

    aula 21-10-25

--- aula-03-consumo-de-api-com-axios/the-games-front-end-react-e-api/front-end-react/src/components/HomeContent/index.js
+++ aula-03-consumo-de-api-com-axios/the-games-front-end-react-e-api/front-end-react/src/components/HomeContent/index.js
@@ -17,7 +17,9 @@ const HomeContent = () => {
     const fetchGames = async() => {
       try{
         const response = await axios.get("http://localhost:4000/games")
-        console.log(response)
+        //Atualizando o estado com a lista de jogos:
+        setGames(response.data.games)
+        //console.log(response.data.games);//Vai aparecer apenas lista de jogos
 
       } catch (error){
         console.log(error);
@@ -26,6 +28,20 @@ const HomeContent = () => {
     fetchGames(); //invocando a função dentro do useEffect
   }, [games]); //antes de finalizar o useEffect o colchete  é a dependência do useEffect (é o que ele fica observando)
 
+
+  //Função de deletar 
+  const deleteGame = async (gameId) => {
+    try {
+      const response = await axios.delete(`http://localhost:4000/games/${gameId}`);
+      if (response.status == 204){
+        alert ("O jogo foi excluido com sucesso")
+        //Filtrando a lista de jogos, removendo o jogo que foi excluido por meio de sua ID:
+        setGames(games.filter((game) => game._id !== gameId))
+      }
+    } catch (error){
+      console.log(error)
+    }
+  }
   return (
     <>
       <div className={styles.homeContent}>
@@ -35,9 +51,41 @@ const HomeContent = () => {
           <div className={styles.title}>
             <h2>Lista de jogos</h2>
           </div>
-          <Loading />
+          {/*<Loading />*/}
           <div className={styles.games} id={styles.games}>
             {/* Lista de jogos irá aqui */}
+            {games.map((game) => (
+              <ul key={game._id} className={styles.listGames}>
+                <div className={styles.gameImg}>
+                  <img src="images/game_cd_cover.png" alt="Jogo em estoque"/>
+                </div>
+                <div clasName={styles.gameInfo}>
+                  <h3>{game.title}</h3>
+                  <li>Plataforma: {game.descriptions.platform}</li>
+                  <li>Gênero: {game.descriptions.genre}</li>
+                  <li>Classificação: {game.descriptions.rating}</li>
+                  <li>Ano: {game.year}</li>
+                  <li>Preço: {game.price.toLocaleString("pt-br", {
+                    style: "currency",
+                    currency: "BRL"
+                  })}</li>
+                  {/* Botão para deletar: */}
+                  <button className={styles.btnDel}
+                  onClick={() => {
+
+                    const confirmed = window.confirm(
+                      "Deseja mesmo excluir o jogo?"
+                    );
+                    if (confirmed){
+                      deleteGame(game._id);
+                    }
+                    }}
+                  >
+                    Deletar
+                  </button>
+                </div>
+              </ul>
+            ))}
           </div>
         </div>
       </div>
